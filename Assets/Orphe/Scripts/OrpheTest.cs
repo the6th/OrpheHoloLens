5570139ea6928bc2e8193ac5c93d42029a26bbea
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2017-07-25 19:09:11 +0900

    いったんプロジェクトを書き出す。コード編集はVS側でやった方が楽

---
+++ Assets/Orphe/Scripts/OrpheTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+#if WINDOWS_UWP
+using Orphe;
+#endif
+
+public class OrpheTest : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start () {
+        OrpheShoe.ServiceUuid;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
